2850607fc044d4b0bdb785961909009a3fb4136d
Joshua Seet <joshu5797@gmail.com>
2019-09-25 22:56:14 +0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,14 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './home/header/header.component';
 import { SearchBarComponent } from './shared-components/search-bar/search-bar.component';
 import { CategoryListComponent } from './home/category-list/category-list.component';
 import { CategoryItemComponent } from './home/category-list/category-item/category-item.component';
 import { HairPageComponent } from './hair-page/hair-page.component';
+import { InsideHeaderComponent } from './shared-components/inside-header/inside-header.component';
+import { BusinessCardComponent } from './shared-components/business-card/business-card.component'
+
 
 
 @NgModule({
@@ -23,7 +26,10 @@ import { HairPageComponent } from './hair-page/hair-page.component';
     CategoryListComponent,
     CategoryItemComponent,
     SearchBarComponent,
-    HairPageComponent
+    HairPageComponent,
+    InsideHeaderComponent,
+    BusinessCardComponent
+
     
   ],
   imports: [
---
+++ src/app/shared-components/inside-header/inside-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inside-header',
+  templateUrl: './inside-header.component.html',
+  styleUrls: ['./inside-header.component.css']
+})
+export class InsideHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
