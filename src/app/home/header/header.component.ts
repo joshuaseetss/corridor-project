5d48006b08e9734295693f5778c313b356892838
Joshua Seet <joshu5797@gmail.com>
2019-11-01 00:08:04 +0800

    Website is now mobile compatible

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -8,5 +8,26 @@ import { Component } from '@angular/core';
 })
 
 export class HeaderComponent{
-    collapsed = false; 
+    collapsed = false;
+    wrapperClasses = '';
+    overlayClasses = 'overlay';
+    hamburgerClasses = 'hamburger is-closed';
+
+    hamburgerClick() {
+        if (this.collapsed) {
+            this.overlayClasses = 'overlay';
+            this.hamburgerClasses = 'hamburger is-closed';
+            this.collapsed = false;
+        } else {
+            this.overlayClasses = 'overlay visible';
+            this.hamburgerClasses = 'hamburger is-open';
+            this.collapsed = true;
+        }
+
+        if (this.wrapperClasses.indexOf('toggled') > -1) {
+            this.wrapperClasses = '';
+        } else {
+            this.wrapperClasses = 'toggled';
+        }
+    }
 }
