52a02366fcf1ba0649f3ad840f1244c19fa804a4
Joshua Seet <joshu5797@gmail.com>
2019-11-05 20:54:08 +0800

    Auto-generated commit

--- src/app/home/category-list/category.model.ts
+++
@@ -1,9 +0,0 @@
-export class Category {
-    public name: string;
-    public imagePath: string; 
-
-    constructor(name: string, imagePath: string) {
-        this.name = name; 
-        this.imagePath = imagePath; 
-    }
-}
