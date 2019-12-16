596646dfeb7625d8abe1d55fe7c5e7f0f188d02e
CaesarPang <47916695+caesarpjz@users.noreply.github.com>
2019-12-16 18:21:19 +0800

    Revert "Revert "Added backend""

--- src/app/customer-sign-up/customer-sign-up-page/customer-sign-up-page.component.ts
+++ src/app/customer-sign-up/customer-sign-up-page/customer-sign-up-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/auth.service';
 
 @Component({
   selector: 'app-customer-sign-up-page',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerSignUpPageComponent implements OnInit {
 
-  constructor() { }
+  signupForm: FormGroup;
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.signupForm = new FormGroup({
+      firstName: new FormControl('',[Validators.required]),
+      lastName: new FormControl('',[Validators.required]),
+      email: new FormControl('',[Validators.required, Validators.email]),
+      phone: new FormControl('',[Validators.required]),
+      password: new FormControl('',[Validators.required])
+    });
   }
 
+  signup() {
+    const data = {
+      firstName: this.signupForm.get('firstName').value,
+      lastName: this.signupForm.get('lastName').value,
+      email: this.signupForm.get('email').value,
+      phone: this.signupForm.get('phone').value,
+      password: this.signupForm.get('password').value
+    };
+    this.authService.customerSignup(data);
+  }
 }
